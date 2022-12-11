3faf870f3dcc8e02bdfadc985f945b27b9dbc424
codeNdev <riturajchanda788@gmail.com>
2022-12-11 10:53:10 +0530

    inital Commit

---
+++ dev_file.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    cout<<"How to deal with all things around, We must know!"<<endl;
+    return 0;
+}
---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+    cout<<"Strigng ar ktoei t ";
+    cout<<"This world is so nice";
+    return 0;
+}
